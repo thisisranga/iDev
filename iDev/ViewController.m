5d033491845a00161926fcdb787d893574f2e136
Ranga Reddy Nallavelli <rnallave@cisco.com>
2015-03-03 11:52:44 -0500

    Added necessary Splash screens (portrait mode iPhone only) and designed rough version of first screen.

--- iDev/ViewController.m
+++ iDev/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "iDev.h"
 
 @interface ViewController ()
-
+@property(nonatomic, retain) UIImageView *_iDevTheme;
+@property(nonatomic, retain) UIButton *_joinCommunity;
 @end
 
 @implementation ViewController
@@ -18,7 +20,21 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:YES];
+    
+    __iDevTheme = [[UIImageView alloc] initWithFrame:self.view.bounds];
+    __iDevTheme.image = [UIImage imageNamed:@"LaunchImage"];
+    [self.view addSubview:__iDevTheme];
+    
+    __joinCommunity = [UIButton buttonWithType:UIButtonTypeCustom];
+    __joinCommunity.frame = CGRectMake(100,500,175,30);
+    [__joinCommunity setTitle:JOIN_COMMUNITY_BUTTON_TITLE forState:UIControlStateNormal];
+    [__joinCommunity setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.view addSubview:__joinCommunity];
+    
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
---
+++ iDev/iDev.h
@@ -0,0 +1,14 @@
+//
+//  iDev.h
+//  iDev
+//
+//  Created by rnallave on 3/3/15.
+//  Copyright (c) 2015 Ranga. All rights reserved.
+//
+
+#ifndef iDev_iDev_h
+#define iDev_iDev_h
+
+#define JOIN_COMMUNITY_BUTTON_TITLE @"Join iDev Community"
+
+#endif
